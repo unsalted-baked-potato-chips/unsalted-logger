1af196ae343e0677fe1d7883ae676a15780f179d
Linux User <robert@DESKTOP-QS3F3ME.localdomain>
2022-01-02 07:32:31 +0000

    basic logging

---
+++ include/ulogger.h
@@ -0,0 +1,23 @@
+#ifndef UNSALTED_ULOGGER_H
+#define UNSALTED_ULOGGER_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+struct _logger_t {
+    FILE* file;
+    char* level_prefix[];
+};
+
+struct _logger_t logger_t;
+
+static inline logger_log(logger_t * logger, size_t log_level, char fmt[], ...){
+    fputs(logger->level_prefix[log_level]);
+
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(logger->file, fmt, args);
+    va_end(args);
+}
+
+#endif //UNSALTED_ULOGGER_H
